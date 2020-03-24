0cb4168ce6f90ade5a0fd42b56be4045057d4a64
tarun nagpal <tarun.nagpal78@gmail.com>
2020-03-24 17:59:57 +0530

    Intial Commit

---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,337 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  LinearProgress,
+  Select,
+  OutlinedInput,
+  MenuItem,
+} from "@material-ui/core";
+import { useTheme } from "@material-ui/styles";
+import {
+  ResponsiveContainer,
+  ComposedChart,
+  AreaChart,
+  LineChart,
+  Line,
+  Area,
+  PieChart,
+  Pie,
+  Cell,
+  YAxis,
+  XAxis,
+} from "recharts";
+
+// styles
+import useStyles from "./styles";
+
+// components
+import mock from "./mock";
+import Widget from "../../components/Widget";
+import PageTitle from "../../components/PageTitle";
+import { Typography } from "../../components/Wrappers";
+import Dot from "../../components/Sidebar/components/Dot";
+import Table from "./components/Table/Table";
+import BigStat from "./components/BigStat/BigStat";
+
+const mainChartData = getMainChartData();
+const PieChartData = [
+  { name: "Group A", value: 400, color: "primary" },
+  { name: "Group B", value: 300, color: "secondary" },
+  { name: "Group C", value: 300, color: "warning" },
+  { name: "Group D", value: 200, color: "success" },
+];
+
+export default function Dashboard(props) {
+  var classes = useStyles();
+  var theme = useTheme();
+
+  // local
+  var [mainChartState, setMainChartState] = useState("monthly");
+
+  return (
+    <>
+      <PageTitle title="Dashboard"  />
+      <Grid container spacing={4}>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Widget
+            title="Visits Today"
+            upperTitle
+            bodyClass={classes.fullHeightBody}
+            className={classes.card}
+          >
+            <div className={classes.visitsNumberContainer}>
+              <Typography size="xl" weight="medium">
+                12, 678
+              </Typography>
+              <LineChart
+                width={55}
+                height={30}
+                data={[
+                  { value: 10 },
+                  { value: 15 },
+                  { value: 10 },
+                  { value: 17 },
+                  { value: 18 },
+                ]}
+                margin={{ left: theme.spacing(2) }}
+              >
+                <Line
+                  type="natural"
+                  dataKey="value"
+                  stroke={theme.palette.success.main}
+                  strokeWidth={2}
+                  dot={false}
+                />
+              </LineChart>
+            </div>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography color="text" colorBrightness="secondary">
+                  Registrations
+                </Typography>
+                <Typography size="md">860</Typography>
+              </Grid>
+              <Grid item>
+                <Typography color="text" colorBrightness="secondary">
+                  Sign Out
+                </Typography>
+                <Typography size="md">32</Typography>
+              </Grid>
+              <Grid item>
+                <Typography color="text" colorBrightness="secondary">
+                  Rate
+                </Typography>
+                <Typography size="md">3.25%</Typography>
+              </Grid>
+            </Grid>
+          </Widget>
+        </Grid>
+        <Grid item lg={3} md={8} sm={6} xs={12}>
+          <Widget
+            title="App Performance"
+            upperTitle
+            className={classes.card}
+            bodyClass={classes.fullHeightBody}
+          >
+            <div className={classes.performanceLegendWrapper}>
+              <div className={classes.legendElement}>
+                <Dot color="warning" />
+                <Typography
+                  color="text"
+                  colorBrightness="secondary"
+                  className={classes.legendElementText}
+                >
+                  Integration
+                </Typography>
+              </div>
+              <div className={classes.legendElement}>
+                <Dot color="primary" />
+                <Typography
+                  color="text"
+                  colorBrightness="secondary"
+                  className={classes.legendElementText}
+                >
+                  SDK
+                </Typography>
+              </div>
+            </div>
+            <div className={classes.progressSection}>
+              <Typography
+                size="md"
+                color="text"
+                colorBrightness="secondary"
+                className={classes.progressSectionTitle}
+              >
+                Integration
+              </Typography>
+              <LinearProgress
+                variant="determinate"
+                value={30}
+                classes={{ barColorPrimary: classes.progressBar }}
+                className={classes.progress}
+              />
+            </div>
+            <div>
+              <Typography
+                size="md"
+                color="text"
+                colorBrightness="secondary"
+                className={classes.progressSectionTitle}
+              >
+                SDK
+              </Typography>
+              <LinearProgress
+                variant="determinate"
+                value={55}
+                classes={{ barColorPrimary: classes.progressBar }}
+                className={classes.progress}
+              />
+            </div>
+          </Widget>
+        </Grid>
+        <Grid item lg={3} md={8} sm={6} xs={12}>
+          <Widget
+            title="Server Overview"
+            upperTitle
+            className={classes.card}
+            bodyClass={classes.fullHeightBody}
+          >
+            <div className={classes.serverOverviewElement}>
+              <Typography
+                color="text"
+                colorBrightness="secondary"
+                className={classes.serverOverviewElementText}
+              >
+                60% / 37°С / 3.3 Ghz
+              </Typography>
+              <div className={classes.serverOverviewElementChartWrapper}>
+                <ResponsiveContainer height={50} width="99%">
+                  <AreaChart data={getRandomData(10)}>
+                    <Area
+                      type="natural"
+                      dataKey="value"
+                      stroke={theme.palette.secondary.main}
+                      fill={theme.palette.secondary.light}
+                      strokeWidth={2}
+                      fillOpacity="0.25"
+                    />
+                  </AreaChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+            <div className={classes.serverOverviewElement}>
+              <Typography
+                color="text"
+                colorBrightness="secondary"
+                className={classes.serverOverviewElementText}
+              >
+                54% / 31°С / 3.3 Ghz
+              </Typography>
+              <div className={classes.serverOverviewElementChartWrapper}>
+                <ResponsiveContainer height={50} width="99%">
+                  <AreaChart data={getRandomData(10)}>
+                    <Area
+                      type="natural"
+                      dataKey="value"
+                      stroke={theme.palette.primary.main}
+                      fill={theme.palette.primary.light}
+                      strokeWidth={2}
+                      fillOpacity="0.25"
+                    />
+                  </AreaChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+            <div className={classes.serverOverviewElement}>
+              <Typography
+                color="text"
+                colorBrightness="secondary"
+                className={classes.serverOverviewElementText}
+              >
+                57% / 21°С / 3.3 Ghz
+              </Typography>
+              <div className={classes.serverOverviewElementChartWrapper}>
+                <ResponsiveContainer height={50} width="99%">
+                  <AreaChart data={getRandomData(10)}>
+                    <Area
+                      type="natural"
+                      dataKey="value"
+                      stroke={theme.palette.warning.main}
+                      fill={theme.palette.warning.light}
+                      strokeWidth={2}
+                      fillOpacity="0.25"
+                    />
+                  </AreaChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+          </Widget>
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Widget title="Revenue Breakdown" upperTitle className={classes.card}>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <ResponsiveContainer width="100%" height={144}>
+                  <PieChart margin={{ left: theme.spacing(2) }}>
+                    <Pie
+                      data={PieChartData}
+                      innerRadius={45}
+                      outerRadius={60}
+                      dataKey="value"
+                    >
+                      {PieChartData.map((entry, index) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={theme.palette[entry.color].main}
+                        />
+                      ))}
+                    </Pie>
+                  </PieChart>
+                </ResponsiveContainer>
+              </Grid>
+              <Grid item xs={6}>
+                <div className={classes.pieChartLegendWrapper}>
+                  {PieChartData.map(({ name, value, color }, index) => (
+                    <div key={color} className={classes.legendItemContainer}>
+                      <Dot color={color} />
+                      <Typography style={{ whiteSpace: "nowrap" }}>
+                        &nbsp;{name}&nbsp;
+                      </Typography>
+                      <Typography color="text" colorBrightness="secondary">
+                        &nbsp;{value}
+                      </Typography>
+                    </div>
+                  ))}
+                </div>
+              </Grid>
+            </Grid>
+          </Widget>
+        </Grid> 
+      </Grid>
+    </>
+  );
+}
+
+// #######################################################################
+function getRandomData(length, min, max, multiplier = 10, maxDiff = 10) {
+  var array = new Array(length).fill();
+  let lastValue;
+
+  return array.map((item, index) => {
+    let randomValue = Math.floor(Math.random() * multiplier + 1);
+
+    while (
+      randomValue <= min ||
+      randomValue >= max ||
+      (lastValue && randomValue - lastValue > maxDiff)
+    ) {
+      randomValue = Math.floor(Math.random() * multiplier + 1);
+    }
+
+    lastValue = randomValue;
+
+    return { value: randomValue };
+  });
+}
+
+function getMainChartData() {
+  var resultArray = [];
+  var tablet = getRandomData(31, 3500, 6500, 7500, 1000);
+  var desktop = getRandomData(31, 1500, 7500, 7500, 1500);
+  var mobile = getRandomData(31, 1500, 7500, 7500, 1500);
+
+  for (let i = 0; i < tablet.length; i++) {
+    resultArray.push({
+      tablet: tablet[i].value,
+      desktop: desktop[i].value,
+      mobile: mobile[i].value,
+    });
+  }
+
+  return resultArray;
+}
---
+++ src/pages/tables/Tables.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import MUIDataTable from "mui-datatables";
+
+// components
+import PageTitle from "../../components/PageTitle";  
+
+const datatableData = [
+  {
+      "id": 1410,
+      "price": 490000,
+      "unitSize": "",
+      "name": "Huggies Gold Pants Popok Celana XL38 (2 Pack)",
+      "sellerName": "Ibu and Bayi",
+      "sellerLocation": "KOTA JAKARTA UTARA",
+      "category": "Kebutuhan Primer",
+      "description": {
+          "tokopedia_url": "https://shopee.co.id/Huggies-Gold-Pants-Popok-Celana-XL38-(2-Pack)-i.4350106.2138584728",
+          "merchantInfo": "",
+          "orderInfo": "",
+          "description": "HUGGIES Gold Pants Popok Celana XL38 \r\nSize = XL \r\nIsi per pack = 38 x 2 Pack\r\nCocok untuk berat badan = 12-17 kg\r\nHuggies Gold Pants merupakan rangkaian produk Huggies kelas premium. Huggies Gold Pants merupakan popok celana sekali pakai yang membuat bayi merasa nyaman saat menggunakannya. Berikut ini keunggulan dari produk Huggies Gold Pants:\r\n1. Mampu menyerap selama 12 jam (semalaman), sangat nyaman di gunakan oleh bayi pada saat tidur dimalam hari\r\n2. Design khusus untuk kenyamanan bayi disekitar pinggang dan kaki\r\n3. Memiliki indikator kebasahan, motif berwarna kuning pada bagian tengah akan berubah menjadi warna biru ketika bayi buang air kecil untuk mengidentifikasi bayi mengganti popoknya. \r\n4. Memiliki 5 lapisan kering dengan cairan penyerap cepat untuk mengunci urine yg masuk dan membatu untuk memastikan kekeringan pada permukaan popok\r\n5. Memiliki 2 lapisan lembut pada kaki untuk melidungi paha bayi dan sangat efektif untuk melindungi dari kebocoran\r\n6. Bagian atas yang luas dan juga bernapas yang memungkinkan sirkulasi udara. Menjaga bagian pinggang dan bawah bayi berventilasi dengan baik\r\n7. Memiliki bahan utama yang sangat lembut menutupi kulit bayi  Shelf Life 36 bulan",
+          "delivery_options": "",
+          "weight": "",
+          "review": {
+              "count": 15,
+              "star": 4.230769
+          },
+          "variantInfo": {}
+      },
+      "quantity": 45,
+      "rating": "4.230",
+      "ratingCount": 15,
+      "itemCondition": "Baru",
+      "merchantSuccessTransaction": "",
+      "merchantItemSold": "",
+      "images": [
+          "https://cf.shopee.co.id/file/b45ecb9154884ec4bd925e5c90a47910",
+          "https://cf.shopee.co.id/file/ff34611af013e798a858144ad3a92d27",
+          "https://cf.shopee.co.id/file/e6106781701f146816939b92f1b87673",
+          "https://cf.shopee.co.id/file/77e9a3ee6e61e9e4568378e61d384d87",
+          "https://cf.shopee.co.id/file/170aee29fbe4926dc7ed4accf5735d73"
+      ],
+      "breadcrumb": "Ibu & Bayi,Popok Bayi,Popok Sekali Pakai",
+      "trending": 1,
+      "inStock": true,
+      "soloPrice": 490000,
+      "groupPrice": 411100,
+      "itemUrl": "\"https://shopee.co.id/Huggies-Gold-Pants-Popok-Celana-XL38-(2-Pack)-i.4350106.2138584728\""
+  },
+  {
+      "id": 139,
+      "price": 490000,
+      "unitSize": "",
+      "name": "Huggies Gold Pants Popok Celana XL38 (2 Pack)",
+      "sellerName": "Ibu and Bayi",
+      "sellerLocation": "KOTA JAKARTA UTARA",
+      "category": "Ibu dan Bayi",
+      "description": {
+          "tokopedia_url": "https://shopee.co.id/Huggies-Gold-Pants-Popok-Celana-XL38-(2-Pack)-i.4350106.2138584728",
+          "merchantInfo": "",
+          "orderInfo": "",
+          "description": "HUGGIES Gold Pants Popok Celana XL38 \r\nSize = XL \r\nIsi per pack = 38 x 2 Pack\r\nCocok untuk berat badan = 12-17 kg\r\nHuggies Gold Pants merupakan rangkaian produk Huggies kelas premium. Huggies Gold Pants merupakan popok celana sekali pakai yang membuat bayi merasa nyaman saat menggunakannya. Berikut ini keunggulan dari produk Huggies Gold Pants:\r\n1. Mampu menyerap selama 12 jam (semalaman), sangat nyaman di gunakan oleh bayi pada saat tidur dimalam hari\r\n2. Design khusus untuk kenyamanan bayi disekitar pinggang dan kaki\r\n3. Memiliki indikator kebasahan, motif berwarna kuning pada bagian tengah akan berubah menjadi warna biru ketika bayi buang air kecil untuk mengidentifikasi bayi mengganti popoknya. \r\n4. Memiliki 5 lapisan kering dengan cairan penyerap cepat untuk mengunci urine yg masuk dan membatu untuk memastikan kekeringan pada permukaan popok\r\n5. Memiliki 2 lapisan lembut pada kaki untuk melidungi paha bayi dan sangat efektif untuk melindungi dari kebocoran\r\n6. Bagian atas yang luas dan juga bernapas yang memungkinkan sirkulasi udara. Menjaga bagian pinggang dan bawah bayi berventilasi dengan baik\r\n7. Memiliki bahan utama yang sangat lembut menutupi kulit bayi  Shelf Life 36 bulan",
+          "delivery_options": "",
+          "weight": "",
+          "review": {
+              "count": 15,
+              "star": 4.230769
+          },
+          "variantInfo": {}
+      },
+      "quantity": 45,
+      "rating": "4.230",
+      "ratingCount": 15,
+      "itemCondition": "Baru",
+      "merchantSuccessTransaction": "",
+      "merchantItemSold": "",
+      "images": [
+          "https://cf.shopee.co.id/file/b45ecb9154884ec4bd925e5c90a47910",
+          "https://cf.shopee.co.id/file/ff34611af013e798a858144ad3a92d27",
+          "https://cf.shopee.co.id/file/e6106781701f146816939b92f1b87673",
+          "https://cf.shopee.co.id/file/77e9a3ee6e61e9e4568378e61d384d87",
+          "https://cf.shopee.co.id/file/170aee29fbe4926dc7ed4accf5735d73"
+      ],
+      "breadcrumb": "Ibu & Bayi,Popok Bayi,Popok Sekali Pakai",
+      "trending": 1,
+      "inStock": true,
+      "soloPrice": 490000,
+      "groupPrice": 411100,
+      "itemUrl": "\"https://shopee.co.id/Huggies-Gold-Pants-Popok-Celana-XL38-(2-Pack)-i.4350106.2138584728\""
+  }
+];
+
+export default function Tables() {
+  return (
+    <>
+      <PageTitle title="Items" />
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <MUIDataTable
+            title="Employee List"
+            data={datatableData}
+
+            data={datatableData.map(item => {
+              return [
+                  item.id,
+                  item.price,
+                  item.sellerName,
+                  item.soloPrice,
+              ]
+          })}
+            columns={["id", "price", "sellerName", "soloPrice"]}
+            options={{
+              filterType: "checkbox",
+            }}
+          />
+        </Grid> 
+      </Grid>
+    </>
+  );
+}
