1f69b3459b20e155a5b4eb571aa72dff34b24980
Vishal <cs_dev1@kitabeli.id>
2020-03-27 20:00:43 +0530

    API pending

--- src/pages/tables/components/AddItem.js
+++ src/pages/tables/components/AddItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
 import { Grid } from "@material-ui/core";
 import MUIDataTable from "mui-datatables";
@@ -16,6 +16,42 @@ import Button from '@material-ui/core/Button';
 import PageTitle from "../../../components/PageTitle";  
 
 const Additem = () => {
+    
+    // const handleSubmit = () => {
+    //     var data = new FormData();
+    //     const payload = {
+    //     id: id.id.getValue(),
+    //     price: id.price.getValue(),
+    //     soloPrice: id.soloPrice.getValue(),
+    //     groupPrice: id.groupPrice.getValue(),
+    //     unitSize: id.unitSize.getValue(),
+    //     name: id.name.getValue(),
+    //     sellerName: id.sellerName.getValue(),
+    //     sellerLocation: id.sellerLocation.getValue(),
+    //     category: id.category.getValue(),
+    //     quantity: id.quantity.getValue(),
+    //     rating: id.rating.getValue(),
+    //     ratingCount: id.ratingCount.getValue(),
+    //     itemCondition: id.itemCondition.getValue(),
+    //     transactionStatus: id.transactionStatus.getValue(),
+    //     merchantItemSold: id.itemSold.getValue(),
+    //     trending: id.trending.getValue(),
+    //     inStock: id.inStock.getValue(),
+    //     description: id.description.getValue(),
+    // };
+    // data.append("myjsonkey", JSON.stringify(payload));
+
+    useEffect(() => {
+        // const requestOptions = {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+        // };
+        // fetch('https://jsonplaceholder.typicode.com/posts', requestOptions)
+        //     .then(response => response.json())
+        //     .then(data => setPostId(data.id));
+    },[])
+
     return(
         <>
         <div>
@@ -24,203 +60,190 @@ const Additem = () => {
             </Link>
             <PageTitle title="Add new item" />
         </div>
-        
-    <TableContainer className="tabelContainer" style={{padding:"50px 20px"}} >
+        <TableContainer className="tabelContainer" style={{padding:"50px 20px"}} >
         {/* first grid */}
-    <Grid container  className="add_itemContainer" >         
-        <Grid item xs={4}   className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Id"
-          multiline
-          variant="outlined"/>
+        <Grid container  className="add_itemContainer" >         
+            <Grid item xs={4}   className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="id"          
+                placeholder="Id"
+                multiline
+                variant="outlined"
+                />
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="price"          
+                placeholder="Price"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="soloPrice"          
+                placeholder="Solo Price"
+                multiline
+                variant="outlined"/>
+            </Grid>
         </Grid>
-
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Price"
-          multiline
-          variant="outlined"/>
-        </Grid>
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Solo Price"
-          multiline
-          variant="outlined"/>
-        </Grid>
-           </Grid>
-           {/* second grid */}
+        {/* second grid */}
                    
-    <Grid container  className="add_itemContainer" >         
-        <Grid item xs={4}   className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Group Price"
-          multiline
-          variant="outlined"/>
+        <Grid container  className="add_itemContainer" >         
+            <Grid item xs={4}   className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="groupPrice"          
+                placeholder="Group Price"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="unitPrice"          
+                placeholder="Unit Size"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="name"          
+                placeholder="Name"
+                multiline
+                variant="outlined"/>
+            </Grid>
         </Grid>
-
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Unit Size"
-          multiline
-          variant="outlined"/>
-        </Grid>
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Name"
-          multiline
-          variant="outlined"/>
-        </Grid>
-           </Grid>
            {/* third grid */}
                    
-    <Grid container  className="add_itemContainer" >         
-        <Grid item xs={4}   className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Seller Name"
-          multiline
-          variant="outlined"/>
+        <Grid container  className="add_itemContainer" >         
+            <Grid item xs={4}   className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="sellerName"          
+                placeholder="Seller Name"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="sellerLocation"          
+                placeholder="Seller Location"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="category"          
+                placeholder="Category"
+                multiline
+                variant="outlined"/>
+            </Grid>
         </Grid>
-
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Seller Location"
-          multiline
-          variant="outlined"/>
-        </Grid>
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Category"
-          multiline
-          variant="outlined"/>
-        </Grid>
-           </Grid>
-           {/* fourth grid */}
+        {/* fourth grid */}
                    
-    <Grid container  className="add_itemContainer" >         
-        <Grid item xs={4}   className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Quantity"
-          multiline
-          variant="outlined"/>
+        <Grid container  className="add_itemContainer" >         
+            <Grid item xs={4}   className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="quantity"          
+                placeholder="Quantity"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="rating"          
+                placeholder="Rating"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="ratingCount"          
+                placeholder="Rating Count"
+                multiline
+                variant="outlined"/>
+            </Grid>
         </Grid>
-
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Rating"
-          multiline
-          variant="outlined"/>
-        </Grid>
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Rating Count"
-          multiline
-          variant="outlined"/>
-        </Grid>
-           </Grid>
 
            {/* fifth grid */}
                  
-    <Grid container  className="add_itemContainer"  >         
-        <Grid item xs={4}   className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Item Condition"
-          multiline
-          variant="outlined"/>
+        <Grid container  className="add_itemContainer"  >         
+            <Grid item xs={4}   className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="itemCondition"          
+                placeholder="Item Condition"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="transactionStatus"          
+                placeholder="Transaction  Status"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="itemSold"          
+                placeholder="Merchant item sold "
+                multiline
+                variant="outlined"/>
+            </Grid>
         </Grid>
-
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Transaction  Status"
-          multiline
-          variant="outlined"/>
-        </Grid>
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Merchant item sold "
-          multiline
-          variant="outlined"/>
-        </Grid>
-           </Grid>
            {/* sixth grid */}
                    {/* first grid */}
-    <Grid container  className="add_itemContainer" >         
-        <Grid item xs={4}   className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Trending "
-          multiline
-          variant="outlined"/>
+        <Grid container  className="add_itemContainer" >         
+            <Grid item xs={4}   className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="trending"          
+                placeholder="Trending "
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="inStock"          
+                placeholder="In stock"
+                multiline
+                variant="outlined"/>
+            </Grid>
+            <Grid item xs={4} className="add_fieldCol">
+            </Grid>
         </Grid>
 
-        <Grid item xs={4} className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="In stock"
-          multiline
-          variant="outlined"/>
-        </Grid>
-        <Grid item xs={4} className="add_fieldCol">
-        
-        </Grid>
-           </Grid>
-
-{/* description container */}
-<Grid container  className="add_itemContainer" >         
-        <Grid item xs={12}   className="add_fieldCol">
-        <TextField
-          className="add_textField"
-          id="outlined-textarea"          
-          placeholder="Description"
-          rows="5"
-          multiline
-          variant="outlined"/>
-        </Grid>
+        {/* description container */}
+        <Grid container  className="add_itemContainer" >         
+            <Grid item xs={12}   className="add_fieldCol">
+                <TextField
+                className="add_textField"
+                id="description"          
+                placeholder="Description"
+                rows="5"
+                multiline
+                variant="outlined"/>
+            </Grid>
         </Grid>
         <Grid container  className="add_itemContainer" >         
         <Grid item xs={12}   className="add_fieldCol">
-       <Button variant="outlined" className="additem_btn" >Browse Image</Button>
+            <Button variant="outlined" className="additem_btn" >Browse Image</Button>
         </Grid>
-        </Grid>
-
-
-
-
+    </Grid>
     </TableContainer>
     </>
-
-        
     )
 }
 
