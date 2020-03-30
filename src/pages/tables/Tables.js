0c4875b25c07868dd13feeb2cf4e2006844d3468
Vishal <cs_dev1@kitabeli.id>
2020-03-30 19:51:03 +0530

    server side pagination

--- src/pages/tables/Tables.js
+++ src/pages/tables/Tables.js
@@ -1,49 +1,61 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, makeStyles} from "react";
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Grid } from "@material-ui/core";
-import MUIDataTable from "mui-datatables";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
+import TableSortLabel from '@material-ui/core/TableSortLabel'
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import EditImg from './Edit_Icon.png';
 import DeatilIcon from './View_Details_Icon.png';
-import iicccoonn  from '../../Icons/Admin_Normal.png';
 
-import {
-  createMuiTheme,
-  MuiThemeProvider,
-  withStyles
-} from "@material-ui/core/styles";
 import { API_URL } from "../../Services"
 // components
 import PageTitle from "../../components/PageTitle";  
 import AddEditItem from "./components/AddEditItem";
 
-export default function Tables() {
+const headCells = [
+  { id: 'id', numeric: true, disablePadding: false, label: 'Id' },
+  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+  { id: 'sellerName', numeric: false, disablePadding: true, label: 'Seller Name' },
+  { id: 'category', numeric: false, disablePadding: true, label: 'Category' },
+  { id: 'description', numeric: false, disablePadding: true, label: 'Description' },
+  { id: 'quantity', numeric: true, disablePadding: false, label: 'Quantity' },
+  { id: 'price', numeric: true, disablePadding: false, label: 'Price' },
+  { id: 'rating', numeric: true, disablePadding: false, label: 'Rating' },
+  { id: 'condition', numeric: false, disablePadding: true, label: 'Condition' },
+  { id: 'transaction', numeric: false, disablePadding: true, label: 'Transaction' },
+  { id: 'actions', numeric: false, disablePadding: true, label: 'Actions' },
+];
+
+export default function Tables(props) {
   const [datatableData, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [url, setUrl] = useState(`${API_URL}/api/v1/int-tool/item?page=0&size=100&sort=price,DESC`);
-
+  const fetchLimit = rowsPerPage*(page+2)
+  
+  const [url, setUrl] = useState(`${API_URL}/api/v1/int-tool/item?page=0&size=${rowsPerPage}&sort=price,DESC`);
+  
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
+    setUrl(`${API_URL}/api/v1/int-tool/item?page=0&size=${fetchLimit}&sort=price,DESC`)
+    fetchData(url);
   };
 
   const handleChangeRowsPerPage = event => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
+    let pageLength = parseInt(event.target.value, 10)
+    setUrl(`${API_URL}/api/v1/int-tool/item?page=0&size=${pageLength}&sort=price,DESC`)
+    fetchData(url);
   };
 
-  /**
-   * Fetch the data on Page Load
-   */
   const fetchData = async () => {
     setIsLoading(true);
      const response = await fetch(url);
@@ -65,19 +77,15 @@ export default function Tables() {
       ) : (
         <TableContainer className="tabelContainer" >
           <Table className="tabel" style={{zoom:'70%'}}>
-            <TableHead >
+            <TableHead>
               <TableRow className="tabelHead_row">
-                <TableCell><span>Id</span></TableCell>
-                <TableCell><span>Name</span></TableCell>
-                <TableCell><span>Seller Name</span></TableCell>
-                <TableCell><span>Category</span></TableCell>
-                <TableCell><span>Description</span></TableCell>
-                <TableCell><span>Quantity</span></TableCell>
-                <TableCell><span>Price</span></TableCell>
-                <TableCell><span>Rating</span></TableCell>
-                <TableCell><span>Condition</span></TableCell>
-                <TableCell><span>Transction</span></TableCell>
-                <TableCell><span>Actions</span></TableCell>
+              {headCells.map((headCell) => (
+              <TableCell
+                key={headCell.id}
+              >
+                {headCell.label}
+              </TableCell>
+        ))}
               </TableRow>
             </TableHead>
             <TableBody className="tabelBody">
@@ -104,7 +112,7 @@ export default function Tables() {
           <TablePagination
             rowsPerPageOptions={[5, 10, 25, 50, { label: 'All', value: -1 }]}
             colSpan={3}
-            count={datatableData.length}
+            count={-1}
             rowsPerPage={rowsPerPage}
             page={page}
             SelectProps={{
@@ -122,5 +130,5 @@ export default function Tables() {
         </Link>
       </Grid>
     </>
-  );
+  ); 
 }
